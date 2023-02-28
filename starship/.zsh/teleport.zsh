380b13729b1da0669ebab789bb5b7cb0b2da8f20
megacron <megacron@d3c3p7.com>
2023-02-28 00:18:55 -0500

    added teleport user

--- starship/.zsh/teleport.zsh
+++ starship/.zsh/teleport.zsh
@@ -1,3 +1,4 @@
 #-------------teleport---------------------------------------------->>>
 export TELEPORT_PROXY=tele.megaport.app
+export TELEPORT_USER=megacron
 #-------------end-of-line------------------------------------------->>>
